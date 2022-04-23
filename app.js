f72453bf53432877e7cf72eac5d7d193a3c3da17
Nick Fulghum <nickfulghum92@gmail.com>
2022-04-23 08:39:09 -0500

    Completed Exercise

---
+++ app.js
@@ -0,0 +1,20 @@
+const PI = 3.14
+
+// What is the differnce between var and let
+// var CAN be reassigned AND redeclared
+// while let CAN be reassigned but NOT redeclared
+// let also creates a blockscoped keyword that only exists 
+// inside a code block
+
+// What is the difference between var and const
+// var can be reassigned and redecalred
+// const can do neither
+// const also exist in a blockscope
+
+// What is the difference between let and const
+// let can be reassigned but not redeclared
+// const can not be reassigned or redeclared
+
+// What is hoisting
+// hoisting moves the declaration of the variables to the 
+// top of their scope prior to the execution
